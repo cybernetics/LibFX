9e6fbe00be5aa1183ccc3173356c1831225156af
nicolaiparlog <nipa@codefx.org>
2015-01-02 22:23:48 +0100

    Create package-info for 'comparer' which doubles as a feature-wide ToDo list.

---
+++ src/main/java/org/codefx/libfx/collection/comparer/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * TODO:<br>
+ * - use code regions (include generic arguments to implemented collections)<br>
+ * - rename package to comparator<br>
+ * - check visibility and finality of classes<br>
+ * - check all comments<br>
+ */
+package org.codefx.libfx.collection.comparer;
